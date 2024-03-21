224092021181e196d1fc0b527b5672415d84c503
Christian Esparza <cesparzajp@gmail.com>
2024-03-21 12:11:12 +0900

    Add logic to update shopping cart
    When a new product is added to the shopping cart, a new objet of the
    class product is created and added to the shopping cart. Additionally
    a remove button and an input for the number of items of the same product
    are shown. I faced the issue of reading an svg from a file, I found that
    I cannot fetch files from my local machine and that a work around would
    require running a local server. Instead I chose to add a dummy element
    with display property set to hidden. Then I clone this element dynamically
    to add an remove copies of it in the right place.

--- scripts/shop-gruel-script.js
+++ scripts/shop-gruel-script.js
@@ -50,7 +50,7 @@ const shoppingCart = {
         totalPrice = this.addedProducts.reduce((total,product) => {
             return total + (product.price * product.quantity);
         },0);
-        return totalPrice;
+        return totalPrice.toFixed(2);
     }
 }
 
@@ -63,17 +63,22 @@ function product(name,price,flavor){
 }
 
 const addButtons = document.querySelectorAll('.add-button');
-addButtons.forEach(button => button.addEventListener('click',addProduct));
+addButtons.forEach(button => button.addEventListener('click',addNewProduct));
 
-function addProduct(event){
-    const button = event.target;
+function addNewProduct(event){
+    //Ensure that the button variable we create refers to the svg element itself
+    //and not the path element inside the svg.
+    const button = event.target.tagName === 'svg' ? event.target : event.target.parentNode;
     const details = getProductDetails(button);
     manageShoppingCart(details);
+    //Remove event listener and show input element
+    button.removeEventListener('click',addNewProduct);
+    showControls(button);
 }
 
-function getProductDetails(button){
+function getProductDetails(element){
     //Look for the flavor container
-    const flavorContainer = button.closest('.flavor');
+    const flavorContainer = element.closest('.flavor');
     //Look for the name of the product
     const name = flavorContainer.closest('.flavors').getAttribute('name');
     //Look for the price
@@ -83,26 +88,108 @@ function getProductDetails(button){
     //Look for the flavor
     const flavor = flavorContainer.querySelector('.product-details p:last-child').textContent;
     // //update quantity
-    // const quantityInput = button.closest('input');
-    // const quantity = quantityInput.value;
-
-    return {name,price,flavor}
+    const quantityInput = flavorContainer.querySelector('input');
+    let quantity;
+    if(quantityInput){
+        quantity = quantityInput.value;
+    }else{
+        quantity = 1;
+    }
+    return {name,price,flavor,quantity}
 }
 
 function manageShoppingCart(details){
     //Check if product already exist on the shopping cart
     //if it does, then increase quantity, otherwise add product
-    const existingProduct = shoppingCart.addedProducts.find(product => 
+
+    const productIndex = shoppingCart.addedProducts.findIndex(product =>
         product.name === details.name && product.flavor === details.flavor
-    );
+        );
     
-    if(existingProduct){
-        existingProduct.quantity++;
-    }else{
+    if(productIndex > -1){ //If the product is in the shopping cart
+        //If quantity = 0, remove product, otherwise update quantity
+        if(details.quantity > 0){
+            shoppingCart.addedProducts[productIndex].quantity = details.quantity;
+        }else{
+            shoppingCart.addedProducts.splice(productIndex,1); // Remove item
+        }
+    }else{ //Add new product to shopping cart
         const newProduct = new product(details.name, details.price, details.flavor);
         shoppingCart.addedProducts.push(newProduct);
     }
     //Update the total price
     shoppingCart.totalPrice=shoppingCart.calculateTotal();
     console.log(shoppingCart);
+}
+
+//Display input and remove-buttons
+//Use input value to update product quantity
+function showControls(button){
+    //Create the div wrapper
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('controls');
+    //Create an input to select the quantity of each product to add to the shopping cart
+    const quantityInput = document.createElement('input');
+    quantityInput.classList.add('control');
+    quantityInput.value = 1;
+    //Use the dummy button to create a copy an insert on the corresponding flavor element
+    const removeButton = document.querySelector('#dummy-button').cloneNode(true); //true makes a deepcopy of the element.
+    removeButton.removeAttribute('id');
+    removeButton.classList.add('remove-button');
+    //Add listeners to modify the number of items to add to the shopping cart
+    button.addEventListener('click',modifyQuantity);
+    removeButton.addEventListener('click',modifyQuantity);
+    quantityInput.addEventListener('change',updateQuantity);
+    //Select the element to insert the wrapper and populate it with the buttons and input
+    const parentElement = button.parentNode;
+    parentElement.insertBefore(wrapper,button);
+    
+    wrapper.appendChild(removeButton);
+    wrapper.appendChild(quantityInput);
+    wrapper.appendChild(button);
+}
+
+function modifyQuantity(event){
+    const button = event.target.tagName === 'svg' ? event.target : event.target.parentNode;
+    const parentElement = button.parentNode;
+    const inputElement = parentElement.querySelector('input');
+    if(button.classList.contains('add-button')){
+        inputElement.value++;
+    }else{
+        inputElement.value--;
+    }
+    updateQuantity(event);
+}
+
+function updateQuantity(event){
+    let element;
+    if(event.value){
+        element = event.target;
+    }else{
+        element = event.target.tagName === 'svg' ? event.target : event.target.parentNode;
+    }
+    const details = getProductDetails(element);
+    manageShoppingCart(details);
+    console.log(details);
+    //If the quantity = 0 then hide the remove button and input
+    if(details.quantity === '0'){
+        hideControls(element);
+    }
+}
+
+function hideControls(element){
+    const parentElement = element.parentNode;
+    const container = parentElement.parentNode;
+    const removeButton = parentElement.querySelector('.remove-button');
+    const inputElement = parentElement.querySelector('input');
+    const addButton = parentElement.querySelector('.add-button');
+    //Reset controls
+    removeButton.removeEventListener('click',modifyQuantity);
+    inputElement.removeEventListener('change',updateQuantity);
+    addButton.removeEventListener('click',modifyQuantity);
+    addButton.addEventListener('click',addNewProduct);
+    //Hide controls
+    parentElement.removeChild(addButton);
+    container.removeChild(parentElement);
+    container.appendChild(addButton);
 }
